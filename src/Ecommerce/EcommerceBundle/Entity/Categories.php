1d46d87066eb670e81c66b937f2a246aafe8f0eb
symfony215 <daniel.green.bigcommerce@gmail.com>
2016-09-11 21:34:28 +0100

    Started making the site dynamic.

---
+++ src/Ecommerce/EcommerceBundle/Entity/Categories.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Ecommerce\EcommerceBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Categories
+ *
+ * @ORM\Table(name="categories")
+ * @ORM\Entity(repositoryClass="Ecommerce\EcommerceBundle\Repository\CategoriesRepository")
+ */
+class Categories
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100, unique=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Ecommerce\EcommerceBundle\Entity\Media",cascade={"persist","remove"})
+     * @ORM\JoinColumn(nullable=false)
+     * */
+    private $media;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Categories
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set media
+     *
+     * @param \Ecommerce\EcommerceBundle\Entity\Media $media
+     *
+     * @return Categories
+     */
+    public function setMedia(Media $media)
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    /**
+     * Get media
+     *
+     * @return \Ecommerce\EcommerceBundle\Entity\Media
+     */
+    public function getMedia()
+    {
+        return $this->media;
+    }
+}
